becfa3a68c8fc9f2849d6cd3788605ada6858e58
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-08-08 17:01:39 +0300

    Test critical css

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
     },
   },
 
+  css: ['@styles/_index.scss'],
   postcss: {
     plugins: {
       'postcss-preset-env': {},
--- src/layouts/PageWrapper.vue
+++ src/layouts/PageWrapper.vue
@@ -5,5 +5,5 @@
 </template>
 
 <style lang="scss">
-@import '@styles';
+// @import '@styles';
 </style>
